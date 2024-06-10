86747f718d358e060b19246728ff5b755acf945b
Ridwan4D <ridwanul161@gmail.com>
2024-06-10 12:27:49 +0600

    jwt is created and middlewares also

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,8 +29,32 @@ async function run() {
     const userCollection = client.db("guideForTourist").collection("users");
     const wishCollection = client.db("guideForTourist").collection("wishlist");
 
+    // ========================================   jwt api start    ========================================
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    // ========================================   jwt api end    ========================================
+    // ========================================   middle wares start    ========================================
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" })
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+    // ========================================   middle wares end    ========================================
     // ========================================   user collection start    ========================================
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
